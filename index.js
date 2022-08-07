125439548ec0d8481e70375270961e39f97ba407
ashutoshgairola <gairola.ashutosh26@.com>
2022-08-07 05:42:17 +0530

    final commit clock

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ closeIcon.addEventListener("click", function () {
 });
 
 // clock
-var i = 1;
+
 const deg = 6;
+var i = 0;
 const hr = document.getElementById("hr");
 const mn = document.getElementById("min");
 
 const sc = document.getElementById("sec");
 
 setInterval(() => {
-  console.log(i++);
   let day = new Date();
   let hh = day.getHours() * 30;
   let mm = day.getMinutes() * deg;
@@ -32,4 +32,10 @@ setInterval(() => {
   hr.classList.add(`rotate-[${hh + mm / 12}deg]`);
   mn.classList.add(`rotate-[${mm}deg]`);
   sc.classList.add(`rotate-[${ss}deg]`);
+  console.log(`${i++} and ${ss} and ${ss - 6}`);
+  if (ss - 6 > 0) sc.classList.remove(`rotate-[${ss - 6}deg]`);
+  if (ss == 6) {
+    sc.classList.remove(`rotate-[0deg]`);
+    sc.classList.remove(`rotate-[354deg]`);
+  }
 }, 1000);
